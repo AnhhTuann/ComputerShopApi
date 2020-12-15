e0f402fbd9475191fb2ca3686fa6d25cbd3f9e10
Phan Dung Tri <phandungtri99@gmail>
2020-12-15 20:23:47 +0700

    set default ticket date

--- DTO/Ticket.cs
+++ DTO/Ticket.cs
@@ -1,10 +1,11 @@
 using System.Collections.Generic;
+using System;
 
 namespace DTO
 {
 	public class Ticket : Entity
 	{
-		public string Date { get; set; }
+		public string Date { get; set; } = DateTime.Now.ToShortDateString();
 		public Staff Staff { get; set; }
 		public int TotalAmount { get; set; } = 0;
 		public List<TicketDetails> Details { get; set; } = new List<TicketDetails>();
